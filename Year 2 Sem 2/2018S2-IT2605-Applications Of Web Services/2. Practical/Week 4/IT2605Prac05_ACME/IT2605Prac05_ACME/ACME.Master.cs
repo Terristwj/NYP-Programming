28433e01d6554650bbe8a8c5a00acf02450fda3f
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:26:38 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 4/IT2605Prac05_ACME/IT2605Prac05_ACME/ACME.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IT2605Prac05_ACME
+{
+    public partial class ACME : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
