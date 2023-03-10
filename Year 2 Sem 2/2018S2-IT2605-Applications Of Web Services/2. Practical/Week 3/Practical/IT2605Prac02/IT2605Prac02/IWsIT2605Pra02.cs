a20cb763f710824021e08dce7653179790d0f5f5
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:11:52 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 3/Practical/IT2605Prac02/IT2605Prac02/IWsIT2605Pra02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IT2605Prac02
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWsIT2605Pra02" in both code and config file together.
+    [ServiceContract]
+    public interface IWsIT2605Pra02
+    {
+        [OperationContract]
+        string HelloWorld(string username);
+
+        [OperationContract]
+        Student GetStudent(int id);
+    }
+}
---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 3/Practical/IT2605Prac02/IT2605Prac02/WsIT2605Pra02.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IT2605Prac02
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WsIT2605Pra02" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WsIT2605Pra02.svc or WsIT2605Pra02.svc.cs at the Solution Explorer and start debugging.
+    public class WsIT2605Pra02 : IWsIT2605Pra02
+    {
+        public string HelloWorld(string username)
+        {
+            return "Hello " + username + ", Welcome to the world of the Web Services";
+        }
+
+        public Student GetStudent(int id)
+        {
+            if (id==1) //Dummy data
+            {
+                Student me = new Student();
+                me.StudentID = id;
+                me.StudentName = "Tan Wei Jun, Terris";
+                me.CourseStudy = "DBI";
+                me.StudentGPA = (decimal)3.81;
+                return me;
+            }
+            else if (id == 2) //Dummy data
+            {
+                Student me = new Student();
+                me.StudentID = id;
+                me.StudentName = "Lee Mei Li";
+                me.CourseStudy = "DBI";
+                me.StudentGPA = (decimal)3.6;
+                return me;
+            }
+            else
+            {
+                Student me = new Student();
+                me.StudentID = id;
+                me.StudentName = "No Such Student";
+                me.CourseStudy = "Nil";
+                me.StudentGPA = (decimal)0;
+                return me;
+            }
+        }
+    }
+}
