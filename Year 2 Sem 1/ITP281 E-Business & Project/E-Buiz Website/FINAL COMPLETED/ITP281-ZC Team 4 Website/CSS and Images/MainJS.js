95c1e6229586ca42e7d0ca3c9313fe667bb834aa
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 00:30:43 +0800

    Add files via upload

---
+++ Year 2 Sem 1/ITP281 E-Business & Project/E-Buiz Website/FINAL COMPLETED/ITP281-ZC Team 4 Website/CSS and Images/MainJS.js
@@ -0,0 +1,21 @@
+﻿function dropdownFunc() {
+    var x = document.getElementsByClassName("nBox0");
+    var y = document.getElementsByClassName("DDNItem");
+    if (x[0].style.height === 43 + 37 * y.length + 2 + "px") {
+        x[0].style.height = "43px";
+    } else {
+        x[0].style.height = 43 + 37 * y.length + 2 + "px";
+    }
+}
+
+function currentPage() {
+    var x = document.getElementsByClassName("DDNItem");
+    var y = document.getElementsByClassName("nItems")
+    var i = getPageNum();
+    if (i !== -1) {
+        x[i].style.background = "rgba(255, 192, 203, 0.75)";
+        y[i].style.background = "rgba(255, 192, 203, 0.75)";
+        //console.log(x[i]);
+        //console.log(y[i]);
+    }
+}
