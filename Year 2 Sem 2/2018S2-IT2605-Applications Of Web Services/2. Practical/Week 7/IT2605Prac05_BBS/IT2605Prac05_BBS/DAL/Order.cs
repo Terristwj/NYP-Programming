3741c5e0c5dc79fe1b2f8f6f43dbde2bf451ef6d
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:46:54 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 7/IT2605Prac05_BBS/IT2605Prac05_BBS/DAL/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace IT2605Prac05_BBS.DAL
+{
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public String ordBookisbn { get; set; }
+
+        [DataMember]
+        public int ordBookQty { get; set; }
+
+    }
+}
