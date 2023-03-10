324532c7f928a3a1c31dc8947d717f50b9b9dc6f
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:50:36 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 8/IT2605Prac05_ACME/IT2605Prac05_ACME/BLL/BllRetailCatalog.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using IT2605Prac05_ACME.DAL;
+
+namespace IT2605Prac05_ACME.BLL
+{
+    public class BllRetailCatalog
+    {
+        private Double calculateRetailPrice(Double rrp)
+        {
+            Double retailPrice;
+            // we reduce the rpp by 5% and use Math.Round to round up the result to 2 decimal places.
+            retailPrice = Math.Round(rrp * 0.95, 2);
+            return retailPrice;
+        }
+        public DataSet getRetailBooksList()
+        {
+            DalBetterBookSupplier dal;
+            DataSet dataSetBooksList;
+            DataTable dt;
+
+            Double rrp;
+            dal = new DalBetterBookSupplier();
+            dataSetBooksList = dal.GetBooks();
+
+            // Business Logic
+            // Acme Book Shop wants to offer the books at 5% below RRP, so we need to write
+            // code here to create a new column in the data and fill it with the new price.
+            //
+            // 1. Get a reeference to the Data Table
+            dt = dataSetBooksList.Tables[0];
+            // 2. Add a new column
+            dt.Columns.Add("RetailPrice", typeof(double));
+            // 3. Check table is not empty
+            if (dt.Rows.Count != 0)
+            {
+                // 4. Loop through each row, calculate the retail price 
+                // and store into the new column
+                foreach(DataRow dr in dt.Rows)
+                {
+                    // Get RRP
+                    rrp = Convert.ToDouble(dr["rrp"]);
+                    // Calculate retail price and store in new column
+                    dr["RetailPrice"] = calculateRetailPrice(rrp);
+
+                }
+
+            }
+            return dataSetBooksList;
+        }
+
+        public DataSet getRetailBookDetails(int bookId)
+        {
+            DalBetterBookSupplier dal;
+            DataSet dataSetBooksList;
+            DataTable dt;
+
+            Double rrp;
+            dal = new DalBetterBookSupplier();
+            dataSetBooksList = dal.GetBookDetails(bookId);
+
+            // Business Logic
+            // Acme Book Shop wants to offer the books at 5% below RRP, so we need to write
+            // code here to create a new column in the data and fill it with the new price.
+            //
+            // 1. Get a reeference to the Data Table
+            dt = dataSetBooksList.Tables[0];
+            // 2. Add a new column
+            dt.Columns.Add("RetailPrice", typeof(double));
+            // 3. Check table is not empty
+            if (dt.Rows.Count != 0)
+            {
+                // 4. Loop through each row, calculate the retail price 
+                // and store into the new column
+                foreach (DataRow dr in dt.Rows)
+                {
+                    // Get RRP
+                    rrp = Convert.ToDouble(dr["rrp"]);
+                    // Calculate retail price and store in new column
+                    dr["RetailPrice"] = calculateRetailPrice(rrp);
+
+                }
+
+            }
+            return dataSetBooksList;
+        }
+    }
+    
+
+}
---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 8/IT2605Prac05_ACME/IT2605Prac05_ACME/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using IT2605Prac05_ACME.BLL;
+using System.Text;
+
+namespace IT2605Prac05_ACME
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+                BindBooksList();
+        }
+        private void BindBooksList()
+        {
+            try
+            {
+                BllRetailCatalog myCat = new BllRetailCatalog();
+                DataSet ds;
+                ds = myCat.getRetailBooksList();
+                GridViewBookList.DataSource = ds;
+                GridViewBookList.DataBind();
+            }
+            catch (Exception e)
+            {
+                showAlert(e.Message + ". Please inform administrator");
+            }
+        }
+
+        protected void GridViewBookList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedProductId = Convert.ToInt32(GridViewBookList.SelectedValue);
+            BllRetailCatalog myCat = new BllRetailCatalog();
+            DataSet ds;
+            ds = myCat.getRetailBookDetails(selectedProductId);
+            DetailsViewBookDetails.DataSource = ds;
+            DetailsViewBookDetails.DataBind();
+        }
+
+        private void showAlert(string exceptMsg)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<script type = 'text/javascript'>");
+            sb.Append("window.onload=function(){");
+            sb.Append("alert('");
+            sb.Append(exceptMsg);
+            sb.Append("')};");
+            sb.Append("</script>");
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", sb.ToString());
+        }
+    }
+}
