cc85dd8fd2a3e67470cb80d4e1336568cc3bd0b7
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:30:38 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 4/IT2605Prac05_BBS/IT2605Prac05_BBS/BLL/BllBookCatalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using IT2605Prac05_BBS.DAL;
+
+namespace IT2605Prac05_BBS.BLL
+{
+    public class BLLBookCatalog
+    {
+        public DataSet GetBooks()
+        {
+            DalBooks dataLayerBooks;
+
+            dataLayerBooks = new DalBooks();
+            return dataLayerBooks.GetAll();
+        }
+    }
+}
