1bb55ff06a604249a79f70175ec73ba013763ee0
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 02:15:15 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/5. Practical Test/RedStoreWeb/RedStoreWeb/DAL/DAL_WsSales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using RedStoreWeb.SvcRedStore;
+
+namespace RedStoreWeb.DAL
+{
+    public class DAL_WsSales
+    {
+        public DataSet GetAllSales()
+        {
+            WsSalesClient redStore = new WsSalesClient();
+            return redStore.getAllSales();
+        }
+
+        public DataSet GetSalesByStaff(string staffID)
+        {
+            WsSalesClient redStore = new WsSalesClient();
+            return redStore.getSalesByStaff(staffID);
+        }
+    }
+}
---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/5. Practical Test/RedStoreWeb/RedStoreWeb/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RedStoreWeb.BLL;
+
+namespace RedStoreWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                bindGV();
+            }
+        }
+
+        private void bindGV()
+        {
+            BLL_WsSales sales = new BLL_WsSales();
+            DataSet ds = sales.getAllSales();
+
+            GV_AllSales.DataSource = ds;
+            GV_AllSales.DataBind();
+        }
+
+        protected void GV_AllSales_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bindDV(GV_AllSales.SelectedValue.ToString());
+        }
+
+        private void bindDV(string value)
+        {
+            BLL_WsSales sales = new BLL_WsSales();
+            DataSet ds = sales.getSalesByStaff(value);
+
+            DV_StaffSales.DataSource = ds;
+            DV_StaffSales.DataBind();
+        }
+    }
+}
