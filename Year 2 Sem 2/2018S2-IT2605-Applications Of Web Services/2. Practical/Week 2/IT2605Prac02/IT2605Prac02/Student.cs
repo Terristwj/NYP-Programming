7848c523341ff85c5ec229046be57e5bef348f83
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:07:30 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 2/IT2605Prac02/IT2605Prac02/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace IT2605Prac02
+{
+    public class Student
+    {
+        [DataMember]
+        public int StudentID { get; set; }
+
+        [DataMember]
+        public string StudentName { get; set; }
+
+        [DataMember]
+        public string CourseStudy { get; set; }
+
+        [DataMember]
+        public decimal StudentGPA { get; set; }
+    }
+}
