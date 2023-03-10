601e43686dad618f8719c4452ba6917729900f4c
Terris <27739101+Terristwj@users.noreply.github.com>
2023-03-11 01:37:21 +0800

    Add files via upload

---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 5/IT2605Prac05_ACME/IT2605Prac05_ACME/BLL/BllRetailCatalog.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using IT2605Prac05_ACME.DAL;
+
+namespace IT2605Prac05_ACME.BLL
+{
+    public class BllRetailCatalog
+    {
+        private Double calculateRetailPrice(Double rrp)
+        {
+            Double retailPrice;
+
+            retailPrice = Math.Round(rrp * 0.95, 2);
+            return retailPrice;
+        }
+
+        public DataSet getRetailBooksList()
+        {
+            DalBetterBookSupplier dal;
+            DataSet dataSetBooksList;
+            DataTable dt;
+            Double rrp;
+
+            dal = new DalBetterBookSupplier();
+            dataSetBooksList = dal.GetBooks();
+
+            //Get reference and put into Data Table
+            dt = dataSetBooksList.Tables[0];
+
+            //Add column into Data Table
+            dt.Columns.Add("RetailPrice", typeof(double));
+
+            //Check table not empty
+            if (dt.Rows.Count != 0)
+            {
+                //Lopp through each row to caluclate the retail price and store into the new column
+                foreach (DataRow dr in dt.Rows)
+                {
+                    //Get RRP (Readjusted Retail Price? I forgot)
+                    rrp = Convert.ToDouble(dr["rrp"]);
+
+                    //Calculate retail price and store in new column
+                    dr["RetailPrice"] = calculateRetailPrice(rrp);
+                }
+            }
+
+            return dataSetBooksList;
+        }
+
+        public DataSet getRetailBookDetails(int bookId)
+        {
+            DalBetterBookSupplier dal;
+            DataSet dataSetBooksList;
+            DataTable dt;
+            Double rrp;
+
+            dal = new DalBetterBookSupplier();
+            dataSetBooksList = dal.GetBookDetails(bookId);
+
+            //Get reference and put into Data Table
+            dt = dataSetBooksList.Tables[0];
+
+            //Add column into Data Table
+            dt.Columns.Add("RetailPrice", typeof(double));
+
+            //Check table not empty
+            if (dt.Rows.Count != 0)
+            {
+                //Lopp through each row to caluclate the retail price and store into the new column
+                foreach (DataRow dr in dt.Rows)
+                {
+                    //Get RRP
+                    rrp = Convert.ToDouble(dr["rrp"]);
+
+                    //Calculate retail price and store in new column
+                    dr["RetailPrice"] = calculateRetailPrice(rrp);
+                }
+            }
+
+            return dataSetBooksList;
+        }
+    }
+}
---
+++ Year 2 Sem 2/2018S2-IT2605-Applications Of Web Services/2. Practical/Week 5/IT2605Prac05_ACME/IT2605Prac05_ACME/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using IT2605Prac05_ACME.BLL;
+
+namespace IT2605Prac05_ACME
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+                BindBooksList();
+        }
+
+        private void BindBooksList()
+        {
+            BllRetailCatalog myCat = new BllRetailCatalog();
+            DataSet ds;
+            ds = myCat.getRetailBooksList();
+            GridViewBookList.DataSource = ds;
+            GridViewBookList.DataBind();
+        }
+
+        protected void GridViewBookList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedProductId = Convert.ToInt32(GridViewBookList.SelectedValue);
+            BllRetailCatalog myCat = new BllRetailCatalog();
+            DataSet ds;
+            ds = myCat.getRetailBookDetails(selectedProductId);
+            DetailsViewBookDetails.DataSource = ds;
+            DetailsViewBookDetails.DataBind();
+        }
+    }
+}
